e90971e09ca1414aaffdc965e0578d4f0e115776
Леонтьев Дмитрий QA-71 <leontyev.goods@gmail.com>
2024-01-27 23:08:38 +0300

    JaCoCotest commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -15,4 +15,15 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+
+    void findincreasMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 10, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
 }
